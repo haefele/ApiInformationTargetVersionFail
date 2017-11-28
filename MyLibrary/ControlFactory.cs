f39f7675c387d4c038b3fbac2f9a5ce5e300d8be
Daniel Häfele <haefele@xemio.net>
2017-11-28 21:56:13 +0100

    Create repro of the issue

---
+++ MyLibrary/ControlFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Metadata;
+using Windows.UI.Xaml.Controls;
+
+namespace MyLibrary
+{
+    public class ControlFactory
+    {
+        public static Control GetNavigationControl()
+        {
+            if (ApiInformation.IsApiContractPresent(typeof(UniversalApiContract).FullName, 5))
+            {
+                return new NavigationView();
+            }
+            else
+            {
+                return new ListView();
+            }
+        }
+    }
+}
